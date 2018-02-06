deafe527f2b206ce3ae788726139d08e206b124f
George Hu <george.hu@gophonebox.com>
2018-02-05 18:12:53 -0800

    update

--- Assets/main.cs
+++ Assets/main.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class main : MonoBehaviour {
+	public GameObject DropPrefab;
+	public GameObject WallPrefab;
+
+	public static float speed = 1f;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		
 	}
 	
