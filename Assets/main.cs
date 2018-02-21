483605581bba7bb1422348c325df0daf54a89611
George Hu <george.hu@gophonebox.com>
2018-02-20 18:07:36 -0800

    update

--- Assets/main.cs
+++ Assets/main.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System;
+using Tetris;
 
 public class Main : MonoBehaviour {
 	public GameObject DropPrefab;
 	public GameObject WallPrefab;
 	public Collider OverlapPrefab;
 
-	public static float speed = 1f;
+	public static int speed = 2;
 	public static float scale = 0.1f;
 	public static int	spx = 6;
 	public static int	spy = 20;
@@ -19,33 +20,70 @@ public class Main : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Awake () {
-		QualitySettings.vSyncCount = 0;  // VSync must be disabled
-		Application.targetFrameRate = 2;
+	void Start () {
+		//QualitySettings.vSyncCount = 0;
+		//Application.targetFrameRate = 2;
+
+		GameObject WallS = Instantiate (WallPrefab);
+		WallS.transform.position = new Vector3 (6, 0, 0)*scale;
+		WallS.transform.localScale = new Vector3 (12, 1, 1)*scale;
+
+		GameObject WallW = Instantiate (WallPrefab);
+		WallW.transform.position = new Vector3 (0, 10, 0)*scale;
+		WallW.transform.localScale = new Vector3 (1, 20, 1)*scale;
+
+		GameObject WallE = Instantiate (WallPrefab);
+		WallE.transform.position = new Vector3 (12, 10, 0)*scale;
+		WallE.transform.localScale= new Vector3 (1,20,1)*scale;
 
 		CreateDrop ();
 	}
 
 	// Update is called once per frame
+	int frame=1;
 	void Update () {
+		frame++;
+		if (frame >= 60) {
+			frame = 0;
+		}
+
 		if (Input.GetKeyDown (KeyCode.A)) {
 			MoveDropLeft ();
+			if (CheckOverlap) {
+				MoveDropRight();
+			}
 		} else if (Input.GetKeyDown (KeyCode.D)) {
 			MoveDropRight ();
-		} else if (Input.GetKeyDown (KeyCode.W)) {
-			MoveDropDown ();
+			if (CheckOverlap) {
+				MoveDropLeft();
+			}
 		} else if (Input.GetKeyDown (KeyCode.Space)) {
-			RotateDrop ();
+			RotateDrop (1);
 		}
-		MoveDropDown ();
+
+		if (60 % (60 / speed)) {
+			MoveDropDown ();
+			if (CheckOverlap) {
+				MoveDropUp ();
+				foreach (GameObject k in drop) {
+					k.AddComponent<BoxCollider>();
+				}
+				drop.Clear();
+				CreateDrop ();
+			}
+		}
+
+		frame = (frame >= 60) ? 1 : frame;
 	}
 
 	void CreateDrop(){
 		System.Random rng1 =new System.Random();
 		System.Random rng2 =new System.Random();
 
-		int type=rng1.Next(0, 6);
-		int rotation=rng2.Next(1, 3);
+		//int type=rng1.Next(0, 6);
+		int type = 0; 
+		//int rotation=rng2.Next(1, 3);
+		int rotation=1;
 
 		GameObject drop1 = Instantiate (DropPrefab);
 		GameObject drop2 = Instantiate (DropPrefab);
@@ -71,12 +109,12 @@ public class Main : MonoBehaviour {
 			drop1.transform.position = new Vector3 (spx-1, spy, 0) * scale;
 			drop2.transform.position = new Vector3 (spx, spy, 0) * scale;
 			drop3.transform.position = new Vector3 (spx+1, spy, 0) * scale;
-			drop4.transform.position = new Vector3 (spx, spy, 0) * scale;
+			drop4.transform.position = new Vector3 (spx, spy-1, 0) * scale;
 		} else if (type == 4) {
 			drop1.transform.position = new Vector3 (spx-1, spy, 0) * scale;
 			drop2.transform.position = new Vector3 (spx, spy, 0) * scale;
 			drop3.transform.position = new Vector3 (spx+1, spy, 0) * scale;
-			drop4.transform.position = new Vector3 (spx+1, spy, 0) * scale;
+			drop4.transform.position = new Vector3 (spx+1, spy-1, 0) * scale;
 		} else if (type == 5) {
 			drop1.transform.position = new Vector3 (spx-1, spy, 0) * scale;
 			drop2.transform.position = new Vector3 (spx, spy, 0) * scale;
@@ -103,14 +141,15 @@ public class Main : MonoBehaviour {
 		drop3.transform.parent = drop2.transform;
 		drop4.transform.parent = drop2.transform;
 
-		System.Random rng =new System.Random();
-		//drop2.transform.RotateAround (drop2.transform.position, drop2.transform.forward, Time.deltaTime*90f * rotation);
-		drop2.transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
-
 		drop.Add (drop1);
 		drop.Add (drop2);
 		drop.Add (drop3);
 		drop.Add (drop4);
+
+		//drop2.transform.RotateAround (drop2.transform.position, drop2.transform.forward, Time.deltaTime*90f * rotation);
+		//drop2.transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
+		RotateDrop(rotation);
+
 	}
 
 	void ClearRows(){
@@ -135,51 +174,59 @@ public class Main : MonoBehaviour {
 
 	void MoveDropLeft(){
 		drop [1].transform.position += new Vector3(-1, 0, 0)*scale;
-		if (!CheckOverlap()) {
-			drop [1].transform.position += new Vector3(1, 0, 0)*scale;
-		}
 	}
 
 	void MoveDropRight(){
 		drop [1].transform.position += new Vector3(1, 0, 0)*scale;
-		if (!CheckOverlap()) {
-			drop [1].transform.position += new Vector3(-1, 0, 0)*scale;
-		}
+	}
+
+	void MoveDropUp(){
+		drop [1].transform.position += new Vector3(0, 1, 0)*scale;
 	}
 
 	void MoveDropDown(){
-		int j = 0;
-		foreach (GameObject i in drop) {
-			if (Physics.Raycast(i.transform.position, Vector3.down, 0.5f*scale)) {
-				j++;
-			}
-		}
-
-		if (j == 0) {
-			drop [1].transform.position += new Vector3(0, -1, 0)*scale;
-		} else {
-			foreach (GameObject k in drop) {
-				Rigidbody rb = k.GetComponent(typeof(Rigidbody)) as Rigidbody;
-				rb.isKinematic = true;
-			}
-			drop.Clear();
-			CreateDrop ();
-		}
+		drop [1].transform.position += new Vector3(0, -1, 0)*scale;
 	}
 
-	void RotateDrop(){
-		drop[1].transform.RotateAround (drop[1].transform.position, Vector3.forward, 90f);
+//	void MoveDropDown(){
+//		int j = 0;
+//		foreach (GameObject i in drop) {
+//			if (Physics.Raycast(i.transform.position, Vector3.down, 0.5f*scale)) {
+//				j++;
+//			}
+//		}
+//
+//		if (j == 0) {
+//			drop [1].transform.position += new Vector3(0, -1, 0)*scale;
+//		} else {
+//			foreach (GameObject k in drop) {
+//				//Rigidbody rb = k.GetComponent(typeof(Rigidbody)) as Rigidbody;
+//				//rb.isKinematic = true;
+//				k.AddComponent<BoxCollider>();
+//			}
+//			drop.Clear();
+//			CreateDrop ();
+//		}
+//	}
 
-		if (!CheckOverlap()) {
-			drop[1].transform.RotateAround (drop[1].transform.position, Vector3.forward, -90f);
+	void RotateDrop(int r){
+		foreach (GameObject i  in drop) {
+			//float o = 2250 * Time.deltaTime * r;
+			i.transform.RotateAround (drop [1].transform.position, Vector3.forward, 45f*r);
+		}
+
+		if (CheckOverlap()) {
+			foreach (GameObject i  in drop) {
+				i.transform.RotateAround (drop [1].transform.position, Vector3.forward, 45f*r);
+			}
 		}
 	}
 
 	bool CheckOverlap(){
 		int cl = 0;
 		foreach (GameObject i in drop) {
-			Collider[] c = Physics.OverlapBox (i.transform.position, new Vector3 (1, 1, 1));
-			cl = (c.Length > 1) ? cl : cl + 1;
+			Collider[] c = Physics.OverlapBox (i.transform.position, new Vector3 (1, 1, 1)*scale);
+			cl = (c.Length > 0) ? cl : cl + 1;
 		}
 
 		return (cl > 0);
